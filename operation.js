fa4bdef4b49c7b0be75149fe0a023d623422ece7
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-12-16 18:25:58 +0530

    Added ArrayOperation

---
+++ operation.js
@@ -0,0 +1,105 @@
+//template literal
+// let Firstname='Joe';
+// let LastName='Root';
+// let FullName=`Hi My name is ${Firstname} ${LastName}`;
+// console.log(FullName);
+// let price = 8.99,
+//     tax = 0.1;
+
+// let netPrice = `Net Price:${(price * (1 + tax)).toFixed(2)}`;
+
+// console.log(netPrice);
+
+var multiString;
+// let webpage=` To develop a webpage developer should have the knowledge
+// -HTML
+// -CSS
+// _Javascript`;
+// console.log(webpage);
+
+var Spread_operator;
+// const arr1=[1,2,3];
+// const arr2=[4,5,6];
+// const combined=[...arr1,...arr2];
+// console.log(combined);
+
+// const num=[1,2,3,4,5,6];
+// const [one,two, ...rest]=num;
+// console.log(one);
+// console.log(two);
+// console.log(rest);
+
+// const vehicle={
+//     bike:"Royal Enfield",
+//     car:"Thor"
+// }
+// const updatevehicle={
+//     color:"black"
+// }
+// const up={...vehicle,...updatevehicle};
+// console.log(up);
+
+// var ob1={foo:'bar',x:23}
+// var cloneobj={...ob1};
+// console.log(cloneobj);
+
+ var Structure_assignment;
+ //var games=["Football","Volleyball","Cricket","Hokey"]
+// var [Football,Volleyball,Cricket]=games;
+// console.log(Football);
+
+// var[first, , , last]=games;
+// console.log(first);
+// console.log(last);
+var index_Operator;
+// console.log(2**10);
+
+var object_key_values;
+// var obj={company:"Apple",model:2022};
+// var ky=Object.keys(obj);
+// console.log(ky);
+// var vl=Object.values(obj);
+// console.log(vl);
+
+var arrayoperation;
+//tostringMethod to conver array to string
+ let companyes=["Apple","Meta","Amazon","Google"];
+// console.log(companyes.toString());
+
+//console.log(companyes.join(" "));
+
+//companyes.pop();
+//companyes.push("TCs")
+
+//companyes.shift();
+
+//companyes.unshift("Flipcart");
+//console.log(companyes);
+
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7];
+// let arr3=[8,9,10];
+// let concatarr=arr1.concat(arr2,arr3);
+// console.log(concatarr);
+
+//splice is used to add new item to array
+//companyes.splice(2,0,"BridzeLabz");
+
+// let ct=companyes.slice(1);
+// console.log(ct);
+
+var map;
+// let num1=[10,20,30,40];
+// let num2=num1.map(test);
+// function test(i,j,array){
+//     return i*2;
+// }
+// console.log(num2);
+// let items= new Map([
+//     ["Biryani",200],
+//     ["Kabab",100],
+//     ["Pizza",230]
+// ]);
+//console.log(items.get("Biryani"));
+//console.log(items.set("Noodles",80));
+//console.log(items.size);
